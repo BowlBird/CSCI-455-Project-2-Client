cd9c3aedf2ec0273eb2082b2097f0028e60b45d5
BowlBird <millercarson111@gmail.com>
2023-10-16 21:00:26 -0500

    added animations and scrolling

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -5,6 +6,7 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -13,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TileMode
@@ -76,39 +79,58 @@ fun App() = AppTheme(true) {
                 }
 
                 Row(Modifier.fillMaxSize()) {
-                    Column(Modifier
+                    LazyColumn(Modifier
                         .fillMaxSize()
                         .padding(5.dp)
                         .clip(RoundedCornerShape(5.dp))
                         .weight(1f)
-                        .background(MaterialTheme.colorScheme.background)
-                        .scrollable(rememberScrollState(), orientation = Orientation.Vertical),
+                        .background(MaterialTheme.colorScheme.background),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text("Current Fundraisers", color = MaterialTheme.colorScheme.onBackground)
+                        item {
+                            Text("Current Fundraisers", color = MaterialTheme.colorScheme.onBackground)
+                        }
                         repeat(state.currentFundraisers.size) {
-                            val fundraiser = state.currentFundraisers[it]
-                            Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.CenterVertically) {
-                                Text(modifier = Modifier.padding(5.dp), text = (it + 1).toString(), fontSize = 20.sp, fontWeight = FontWeight.SemiBold, color = MaterialTheme.colorScheme.onBackground)
-                                Fundraiser(fundraiser = fundraiser)
+                            item {
+                                val fundraiser = state.currentFundraisers[it]
+                                Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.CenterVertically) {
+                                    Text(
+                                        modifier = Modifier.padding(5.dp),
+                                        text = (it + 1).toString(),
+                                        fontSize = 20.sp,
+                                        fontWeight = FontWeight.SemiBold,
+                                        color = MaterialTheme.colorScheme.onBackground
+                                    )
+                                    Fundraiser(fundraiser = fundraiser)
+                                }
                             }
                         }
+
                     }
-                    Column(Modifier
+                    LazyColumn(Modifier
                         .fillMaxSize()
                         .padding(5.dp)
                         .clip(RoundedCornerShape(5.dp))
                         .weight(1f)
-                        .background(MaterialTheme.colorScheme.background)
-                        .scrollable(rememberScrollState(), orientation = Orientation.Vertical),
+                        .background(MaterialTheme.colorScheme.background),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text("Old Fundraisers", color = MaterialTheme.colorScheme.onBackground)
+                        item {
+                            Text("Old Fundraisers", color = MaterialTheme.colorScheme.onBackground)
+                        }
                         repeat(state.oldFundraisers.size) {
-                            val fundraiser = state.oldFundraisers[it]
-                            Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.CenterVertically) {
-                                Text(modifier = Modifier.padding(5.dp), text = (it + 1).toString(), fontSize = 20.sp, fontWeight = FontWeight.SemiBold, color = MaterialTheme.colorScheme.onBackground)
-                                Fundraiser(fundraiser = fundraiser)
+                            item {
+                                val fundraiser = state.oldFundraisers[it]
+                                Row(modifier = Modifier.padding(5.dp), verticalAlignment = Alignment.CenterVertically) {
+                                    Text(
+                                        modifier = Modifier.padding(5.dp),
+                                        text = (it + 1).toString(),
+                                        fontSize = 20.sp,
+                                        fontWeight = FontWeight.SemiBold,
+                                        color = MaterialTheme.colorScheme.onBackground
+                                    )
+                                    Fundraiser(fundraiser = fundraiser)
+                                }
                             }
                         }
                     }
@@ -139,21 +161,31 @@ fun Fundraiser(modifier: Modifier = Modifier, fundraiser: Fundraiser) {
             MaterialTheme.colorScheme.tertiary, MaterialTheme.colorScheme.background)
         Text("$${String.format("%,.2f", fundraiser.balance)} raised out of $${String.format("%,.2f", fundraiser.goal)}")
         Text(fundraiser.deadline)
-        if (opened) {
-            var amount by remember { mutableStateOf("") }
-            CustomTextField(modifier = Modifier.fillMaxWidth(), value = amount, onValueChange = {amount = it}, placeholder = {Text("Amount To Donate...")})
-            Button(onClick = {
+        AnimatedVisibility(opened) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                var amount by remember { mutableStateOf("") }
+                CustomTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = amount,
+                    onValueChange = { amount = it },
+                    placeholder = { Text("Amount To Donate...") })
+                Button(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                    shape = RoundedCornerShape(5.dp),
+                    onClick = {
 
-                val viewModel = ViewModel.Factory
+                    val viewModel = ViewModel.Factory
 
-                if(amount.toDoubleOrNull() != null) {
-                    val doubleAmount = amount.toDouble()
-                    viewModel.sendMessage(
-                        "endpoint : DONATE\nid : ${fundraiser.id}\namount : $doubleAmount"
-                    )
+                    if (amount.toDoubleOrNull() != null) {
+                        val doubleAmount = amount.toDouble()
+                        viewModel.sendMessage(
+                            "endpoint : DONATE\nid : ${fundraiser.id}\namount : $doubleAmount"
+                        )
+                    }
+                }) {
+                    Text("Donate!")
                 }
-            }) {
-                Text("Donate!")
             }
         }
     }
